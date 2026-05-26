4d5333b03b98b632e40927c0bec192e7457f0beb
ovidiumuntean <ovidiu.muntean@imawo.ro>
2026-05-26 12:07:34 +0300

    UI Updates

---
+++ Seven Seconds Watch App/Extensions/UIApplication+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Seven Seconds Champions
+//
+//  Created by Ovidiu Muntean on 26/05/2026.
+//
+
+
+import UIKit
+import SwiftUI
+
+// MARK: EXTENSIONS (Culori Neon)
+extension Color {
+    static let neonCyan = Color(red: 0, green: 1, blue: 1)
+    static let neonBlue = Color(red: 0, green: 0.5, blue: 1)
+    static let neonPurple = Color(red: 0.5, green: 0, blue: 1)
+    static let neonRed = Color(red: 1, green: 0.1, blue: 0.1)
+    static let neonOrange = Color(red: 1, green: 0.5, blue: 0)
+    static let deepSpace = Color(red: 0.05, green: 0.05, blue: 0.1)
+}
